6126204c65b47aea3876efce364ea6af14e9401a
Brian Sung <brian.sung.work@gmail.com>
2024-05-13 14:13:13 -0700

    Update Model 1.py

--- Model 1.py
+++ Model 1.py
@@ -1,2 +1,15 @@
-for i in l:
-    
+# Importing necessary libraries
+import pandas as pd
+import yfinance as yf
+
+# Define the ticker symbol
+tickerSymbol = 'AAPL'
+
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
+
+# See your data
+print(tickerDf)
